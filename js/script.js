e95f2e1c4cc57fb4578cbdf63809660b29ba7835
badimala218 <badimala@ucsd.edu>
2026-02-18 23:30:48 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// CREATE AN ARRAY OF EMPLOYEES
+let initialSet = [
+    [11111666, "Demon", 1666, "demon@bor.cor", "Executive"],
+    [11111122, "Razz", 1111, "razz@bor.cor", "Sales"],
+    [11110000, "Battleship", 2222, "bship@bor.cor", "Engineering"],
+    [10000002, "OWO", 3333, "owo@bor.cor", "Marketing"],
+    [55555555, "Unpaid intern", 4545, "intern@bor.cor", "QA"]
+];
+
+// CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
+// IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
+
+
+// GET DOM ELEMENTS
+let table = document.getElementsByTagName("tbody");
+
+// BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
+var buildTable = function() 
+{
+    "use strict";
+    
+}
+
+// ADD EMPLOYEE
+form.addEventListener('submit', (e) => {
+    "use strict";
+    // PREVENT FORM SUBMISSION
+    e.preventDefault();
+
+    // GET THE VALUES FROM THE TEXT BOXES
+
+    // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
+
+    // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
+
+    // BUILD THE GRID
+
+    // RESET THE FORM
+
+    // SET FOCUS BACK TO THE ID TEXT BOX
+
+});
+
+// DELETE EMPLOYEE
+empTable.addEventListener('click', (e) => {
+    // CONFIRM THE DELETE
+
+        // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+
+        // REMOVE EMPLOYEE FROM ARRAY
+
+        // BUILD THE GRID
+
+});
+
+// BUILD THE EMPLOYEES GRID
+function buildGrid() {
+    // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
+
+    // REBUILD THE TBODY FROM SCRATCH
+
+    // LOOP THROUGH THE ARRAY OF EMPLOYEES
+    // REBUILDING THE ROW STRUCTURE
+
+    // BIND THE TBODY TO THE EMPLOYEE TABLE
+
+    // UPDATE EMPLOYEE COUNT
+
+    // STORE THE ARRAY IN STORAGE
+
+};
